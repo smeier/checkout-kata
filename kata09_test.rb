2ffcf90672ad2ca8b22fb048b75a55654c30f5af
smeier <stefan.meier.kiel@gmail.com>
2015-09-11 22:39:04 +0200

    added test for item with more than 3 options

--- kata09_test.rb
+++ kata09_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require_relative 'kata09'
 
-RULES={"A" => {1 => 50, 3 => 130}, "B" => {1 =>30, 2 => 45}, "C" => {1 => 20}, "D" => {1 => 15}}
+RULES={"A" => {1 => 50, 3 => 130}, "B" => {1 =>30, 2 => 45}, "C" => {1 => 20}, "D" => {1 => 15}, "E" => {1 => 20, 3 => 50, 6 => 200}}
 
 class TestPrice < Test::Unit::TestCase
 
@@ -19,7 +19,7 @@ class TestPrice < Test::Unit::TestCase
 
     assert_equal(100, price("AA"))
     assert_equal(130, price("AAA"))
-   assert_equal(180, price("AAAA"))
+    assert_equal(180, price("AAAA"))
     assert_equal(230, price("AAAAA"))
     assert_equal(260, price("AAAAAA"))
 
@@ -27,6 +27,10 @@ class TestPrice < Test::Unit::TestCase
     assert_equal(175, price("AAABB"))
     assert_equal(190, price("AAABBD"))
     assert_equal(190, price("DABABA"))
+  end
+  
+  def test_totals_with_more_than_two_rules_per_item
+      assert_equal(200, price("EEEEEE"))
   end
 
   def test_incremental
