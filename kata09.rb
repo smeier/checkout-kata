5c1339605c2861341973d763175bb6510efd134b
smeier <stefan.meier.kiel@gmail.com>
2015-09-11 22:21:03 +0200

    rules are now a hash of hashes passes all the tests of the original test suite

--- kata09.rb
+++ kata09.rb
@@ -1,14 +1,35 @@
 class CheckOut
     def initialize(rules)
         @rules = rules
-        @total = 0
+        @items = {}
     end
 
     def scan(item)
-        @total += @rules[item]
+        if @items[item]
+            @items[item] += 1
+        else
+            @items[item] = 1
+        end
     end
 
     def total
-        @total
+        return @items.map{|item, count| item_total(item, count)}.inject(0, :+)
+    end
+    
+    def item_total(item, count)
+        total = 0
+        items_left = @items[item]
+        while items_left > 0
+            items_used, cost = take_as_much_items_as_possible(@rules[item], items_left)
+            total += cost
+            items_left -= items_used
+        end
+        total
+    end
+
+    def take_as_much_items_as_possible(rules, items_left)
+        candidate_rules = rules.select{|count, price| count <= items_left}
+        best_rule = candidate_rules.max_by{|count, price| count}
+        best_rule.to_a
     end
 end
--- kata09_test.rb
+++ kata09_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require_relative 'kata09'
 
-RULES={"A" => 50, "B" => 30, "C" => 20, "D" => 15}
+RULES={"A" => {1 => 50, 3 => 130}, "B" => {1 =>30, 2 => 45}, "C" => {1 => 20}, "D" => {1 => 15}}
 
 class TestPrice < Test::Unit::TestCase
 
@@ -18,24 +18,24 @@ class TestPrice < Test::Unit::TestCase
     assert_equal(115, price("CDBA"))
 
     assert_equal(100, price("AA"))
-#    assert_equal(130, price("AAA"))
-#    assert_equal(180, price("AAAA"))
-#    assert_equal(230, price("AAAAA"))
-#    assert_equal(260, price("AAAAAA"))
-#
-#    assert_equal(160, price("AAAB"))
-#    assert_equal(175, price("AAABB"))
-#    assert_equal(190, price("AAABBD"))
-#    assert_equal(190, price("DABABA"))
+    assert_equal(130, price("AAA"))
+   assert_equal(180, price("AAAA"))
+    assert_equal(230, price("AAAAA"))
+    assert_equal(260, price("AAAAAA"))
+
+    assert_equal(160, price("AAAB"))
+    assert_equal(175, price("AAABB"))
+    assert_equal(190, price("AAABBD"))
+    assert_equal(190, price("DABABA"))
   end
 
   def test_incremental
-#    co = CheckOut.new(RULES)
-#    assert_equal(  0, co.total)
-#    co.scan("A");  assert_equal( 50, co.total)
-#    co.scan("B");  assert_equal( 80, co.total)
-#    co.scan("A");  assert_equal(130, co.total)
-#    co.scan("A");  assert_equal(160, co.total)
-#    co.scan("B");  assert_equal(175, co.total)
+    co = CheckOut.new(RULES)
+    assert_equal(  0, co.total)
+    co.scan("A");  assert_equal( 50, co.total)
+    co.scan("B");  assert_equal( 80, co.total)
+    co.scan("A");  assert_equal(130, co.total)
+    co.scan("A");  assert_equal(160, co.total)
+    co.scan("B");  assert_equal(175, co.total)
   end
 end
