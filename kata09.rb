afff6eb08734d22a28d00dfc1210785fadf1aa35
smeier <stefan.meier.kiel@gmail.com>
2015-09-13 19:20:49 +0200

    Rule configurations can now be passed in as a string, formatted like this: Item   Unit      Special        Price     Price --------------------------   A     50       3 for 130   B     30       2 for 45   C     20   D     15

--- kata09.rb
+++ kata09.rb
@@ -1,6 +1,26 @@
 class CheckOut
+    def self.from_config_table(table)
+        self.new(ConfigParser.new.parse(table))
+    end
+
     def initialize(rules)
         @price_calculator = PriceCalculator.new rules
+        @scanner = Scanner.new
+    end
+
+    def total()
+        @price_calculator.total(@scanner.items)
+    end
+
+    def scan(item)
+        @scanner.scan(item)
+    end
+end
+
+class Scanner
+    attr_reader :items
+
+    def initialize()
         @items = {}
     end
 
@@ -12,9 +32,6 @@ class CheckOut
         end
     end
 
-    def total()
-        @price_calculator.total(@items)
-    end
 end
 
 class PriceCalculator
@@ -41,5 +58,34 @@ class PriceCalculator
         candidate_rules = rules.select{|count, price| count <= items_left}
         best_rule = candidate_rules.max_by{|count, price| count}
         best_rule.to_a
+    end
+end
+
+class ConfigParser
+    LINE_REGEX = /(\S+)\s+(\d+)\s+(.*)/
+    DISCOUNT_REGEX = /\S+\s+for\s+\d+\s*/
+
+    def parse(str)
+        valid_rules = str.each_line.select{|line| is_valid_rule(line)}
+        Hash[valid_rules.map{|line| rules_for_one_item(line)}]
+    end
+
+    def is_valid_rule(line)
+        line.match(LINE_REGEX)
+    end
+    
+    def rules_for_one_item(line)
+        rule_parts = line.match(LINE_REGEX).captures
+        item = rule_parts.shift
+        price_for_one = rule_parts.shift.to_i
+        discounts = rule_parts.shift.scan(DISCOUNT_REGEX).flatten
+        rules = Hash[discounts.map{|discount| parse_discount(discount)}]
+        rules[1] = price_for_one
+        [item, rules]
+    end
+
+    def parse_discount(discount)
+        count, price = discount.match(/(\S+)\s+for\s+(\S+)/).captures
+        [count.to_i, price.to_i]
     end
 end
